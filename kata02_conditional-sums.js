162fceac8e01947b6b2c33dfc594ebd45e3e5e2b
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-18 20:14:22 +0000

    final version

---
+++ kata02_conditional-sums.js
@@ -0,0 +1,14 @@
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  for (let i = 0; i < values.length; i++) {
+    if ((condition === 'even' && values[i] % 2 === 0) || (condition === 'odd' && values[i] % 2 !== 0)) {
+      sum += values[i]; 
+    } 
+  }
+  return sum;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
