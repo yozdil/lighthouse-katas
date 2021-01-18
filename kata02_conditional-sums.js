6bddbce48c690c38d31e66aa9bcd6a12baca2c97
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-18 20:18:08 +0000

    Final Version

--- kata02_conditional-sums.js
+++ kata02_conditional-sums.js
@@ -1,6 +1,8 @@
 const conditionalSum = function(values, condition) {
   let sum = 0;
   for (let i = 0; i < values.length; i++) {
+    // In this loop where we iterate through array elements we want to see if a given number is odd or even
+    // At the same time if the condition is met. If one of the conditions in our two part equates to true then we add the value to the sum variable.
     if ((condition === 'even' && values[i] % 2 === 0) || (condition === 'odd' && values[i] % 2 !== 0)) {
       sum += values[i]; 
     } 
