cb0cd2e5c3d6cb3bc18aee66712e10a1a68a10b8
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-18 22:39:01 +0000

    Final Version with the trim() method

--- kata05_percent-encoded-string.js
+++ kata05_percent-encoded-string.js
@@ -1,15 +1,6 @@
 const urlEncode = function (text) {
-  let trimmed = "", encoded = "";
-  // This block checks if there is white space at the first or the last character of the word and eliminates it.
-  if (text[(text.length) - 1] === " " && text[0] === " ") {
-    trimmed = text.slice(1, (text.length - 1))
-  } else if (text[0] === " ") {
-    trimmed = text.slice(1);
-  } else if (text[(text.length) - 1] === " ") {
-    trimmed = text.slice(0, (text.length - 1))
-  } else {
-    trimmed = text;
-  }
+  let trimmed = text.trim();
+  let encoded = "";
   // All the white space inside of the word is changed into %20
   for (let i = 0; i < trimmed.length; i++) {
     if (trimmed[i] === " ") {
