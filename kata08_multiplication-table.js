c525e2d6896c7e028ff01101851ebde226067301
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-19 22:12:34 +0000

    Final Version

---
+++ kata08_multiplication-table.js
@@ -0,0 +1,14 @@
+const multiplicationTable = function (maxValue) {
+  table = "";
+  for (let i = 1; i <= maxValue; i++) {
+    for (let j = 1; j <= maxValue; j++) {
+      table += (j * i + " ")
+    }
+    table += "\n"
+  }
+  return table;
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
