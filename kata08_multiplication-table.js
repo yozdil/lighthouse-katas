c4dbd5b4642f5923ec3422ff21de1e212ec3d7fd
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-19 22:14:58 +0000

    Final Version With Comments

--- kata08_multiplication-table.js
+++ kata08_multiplication-table.js
@@ -2,8 +2,11 @@ const multiplicationTable = function (maxValue) {
   table = "";
   for (let i = 1; i <= maxValue; i++) {
     for (let j = 1; j <= maxValue; j++) {
+      // We want all the values printed to our row, and depending on the column
+      // whole row values are multiplied with.
       table += (j * i + " ")
     }
+    // Before re strating our row we skip to a new line
     table += "\n"
   }
   return table;
