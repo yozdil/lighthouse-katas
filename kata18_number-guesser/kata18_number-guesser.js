5c451f39dc42326711f38d1de0398f978a687a5f
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-23 22:48:26 +0000

    Final Version With comments

--- kata18_number-guesser/kata18_number-guesser.js
+++ kata18_number-guesser/kata18_number-guesser.js
@@ -2,7 +2,7 @@ let prompt = require('prompt-sync')();
 let randomNum = Math.floor(Math.random() * 100) + 1; //So we do not count 0 as a random number.
 let tries = [];
 let eureka = false;
-// THe following function will verify if the answer is already in our array of
+// The following function will verify if the answer is already in our array of
 // previous answers.
 function alreadyGuessed(answer, tries) {
   for (let i = 0; i < tries.length; i++) {
@@ -16,6 +16,10 @@ function alreadyGuessed(answer, tries) {
 while (!eureka) {
   let answer = prompt("Guess a number: ");
   answer = Number(answer);
+  // Prompt brings us back a number but it's in string format so if the entered
+  // prompt is a number it will become a real number. If it was a string not
+  // ressembling a number this will simply not function. So for our first if
+  // statement we can verify if indeed we are dealing with a number or not.
   if (isNaN(answer)) {
     console.log('Not a number! Try again');
   } else if (alreadyGuessed(answer, tries)) {
