b1e39c46eb61c0b78e4b7b51872aab4f20eb05f8
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-23 15:13:33 +0000

    Final Version

--- kata17_taxicab-geometry.js
+++ kata17_taxicab-geometry.js
@@ -1,4 +1,7 @@
 const blocksAway = function (directions) {
+  // First thing first this function WILL fail and show the wrong result if a
+  // complicated path is given where the taxi does loops. The path of the taxi
+  // should never intersect an already traversed path
   let direction = new Array;
   let amount = new Array;
   let output = { east: 0, north: 0 };
@@ -10,33 +13,41 @@ const blocksAway = function (directions) {
       amount.push(directions[i])
     }
   }
-  // console.log(direction);
-  // console.log(amount);
-
+  // We create two arrays: one to decide if we add the amount value  to east or
+  // north, the other is to make sure we get a negative value for the amount
+  // travelled depending on the situation
   let arr = [1, 1];
-  let dir = ['east', 'north'];
-  if (direction[0]==='left') {
-    dir.reverse();
+  let cardinal = ['east', 'north'];
+  // This if statement deals for the set up of our cardinal directions
+  // (cardinal) array. In case if we start with a left we will first go up north
+  // so it reverses the order of the cardinal.
+  if (direction[0] === 'left') {
+    cardinal.reverse();
   }
-
+  // This is the loop where the magic happens. If a certain direction is repeated
+  // (direction[i] = direction[i+1]) we store the preceding value (arr[1]) as negative.
   for (let i = 0; i < direction.length; i++) {
     if (direction[i] === "right") {
-      output[dir[0]] += amount[i] * arr[0];
-      if (direction[i + 1] === "right") {
+      output[cardinal[0]] += amount[i] * arr[0];
+      if (direction[i + 1] === "right") /*If the next turn is also right*/ {
         arr[1] *= -1;
       }
-    } else {
-      output[dir[0]] += amount[i] * arr[0];
-      if (direction[i + 1] === "left") {
+    } else { //If a left turn is made
+      output[cardinal[0]] += amount[i] * arr[0];
+      if (direction[i + 1] === "left") /*If the next turn is also left*/ {
         arr[1] *= -1;
       }
     }
+    // Whatever our turn is the next step the axis changes so north and east
+    // gets switched. An the arr array is also switched, if the preceding value
+    // is negative now our current value will be -1 and will be multiplied with
+    // our amount travelled for the next iteration of the loop.
     arr.reverse();
-    dir.reverse()
+    cardinal.reverse()
   }
   return output;
 };
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
-console.log(blocksAway(["left", 1, "left", 1, "right", 1, "right", 1, "right", 1, "left", 1]));
+console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
 console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
